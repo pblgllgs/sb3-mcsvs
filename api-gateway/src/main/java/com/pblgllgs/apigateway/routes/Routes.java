6aa0ac4319a217641b515f0cc38d1d24642229f1
pblgllgs <pbl.gllgs@gmail.com>
2024-04-17 11:35:02 -0400

    add gateway service

---
+++ api-gateway/src/main/java/com/pblgllgs/apigateway/routes/Routes.java
@@ -0,0 +1,52 @@
+package com.pblgllgs.apigateway.routes;
+/*
+ *
+ * @author pblgl
+ * Created on 17-04-2024
+ *
+ */
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoutes() {
+        return GatewayRouterFunctions
+                .route("product_service")
+                .route(
+                        RequestPredicates.path("/api/product"),
+                        HandlerFunctions.http("http://localhost:8080")
+                )
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoutes() {
+        return GatewayRouterFunctions
+                .route("order_service")
+                .route(
+                        RequestPredicates.path("/api/order"),
+                        HandlerFunctions.http("http://localhost:8081")
+                )
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoutes() {
+        return GatewayRouterFunctions
+                .route("inventory_service")
+                .route(
+                        RequestPredicates.path("/api/inventory"),
+                        HandlerFunctions.http("http://localhost:8082")
+                )
+                .build();
+    }
+}
